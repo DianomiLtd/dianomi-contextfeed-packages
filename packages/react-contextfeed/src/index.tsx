35ebae9189ae1d4f0e1f46d4493844006a4dbdd3
DianomiJH <joe.harrison@dianomi.com>
2022-03-31 09:45:25 +0100

    removed some dependencies fixed tests

--- packages/react-contextfeed/src/index.tsx
+++ packages/react-contextfeed/src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import invariant from 'tiny-invariant';
 
 type Props = {
-  id: String | Number;
+  id: Number;
 };
 
 export const DianomiContextFeed = ({ id }: Props) => {
